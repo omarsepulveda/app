203106cf27c5d8d67797114d1bd744eb50601138
Omar Sepulveda <curaseco@gmail.com>
2017-05-09 11:35:47 -0500

    Se agregan usuarios

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ user: 'tobi' });
+  res.json({ user: ['tobi','Andres','Juan'] });
 });
 
 module.exports = router;
